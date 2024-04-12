55d3fc66b836692898f20e4f5c137548aa56364b
Viacheslav Strogalev <strogalev@hotmail.com>
2024-04-12 22:54:27 +0300

    Codewars 0021 solved

--- codewars/0021_string_replace_to_position.js
+++ codewars/0021_string_replace_to_position.js
@@ -12,5 +12,9 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 */
 
 function alphabetPosition(text) {
-  return text;
+  const reg = /[a-z]/gi;
+  const str = text.toLowerCase().match(reg);
+  return str && str.map(el => el.charCodeAt()-96).join(' ') || '';
 }
+
+console.log(alphabetPosition("{$9(6(:>"))
